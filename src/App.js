d95feffd9215d1e2fa24f407ffc578434dd73cf4
Viktor Miranda <info@viktormiranda.com>
2019-02-02 10:27:11 +0100

    Styles and more content

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@
 'use strict';
 
 import React, {Component} from 'react';
-import { Platform, StyleSheet, Button, Text, View } from 'react-native';
+import { Button, Image, Platform, StyleSheet, Text, View } from 'react-native';
 import { createAppContainer, createStackNavigator } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 
@@ -11,7 +11,7 @@ import CalendarScreen from './Screens/CalendarScreen';
 import AlphabeticScreen from './Screens/AlphabeticScreen';
 import { InfoScreen } from './Screens/InfoScreen';
 import TraditionScreen from './Screens/TraditionScreen';
-
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 class Calendar extends React.Component {
   render() {
@@ -27,7 +27,7 @@ class Alphabetic extends React.Component {
   render() {
     return (
       <View style={baseStyles}>
-        <AlphabeticScreen />
+        <AlphabeticScreen navigation={this.props.navigation} />
       </View>
     );
   }
@@ -56,9 +56,7 @@ class Reminders extends React.Component {
 class Tradition extends React.Component {
   render() {
     return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <TraditionScreen navigation={this.props.navigation} />
-      </View>
+      <TraditionScreen navigation={this.props.navigation} />
     );
   }
 }
@@ -68,41 +66,108 @@ const CalendarStack = createStackNavigator({
     screen: Calendar,
     navigationOptions: () => ({
       title: 'Calendar',
-      headerBackTitle: null
+      headerBackTitle: 'Tillbaka',
+      headerStyle: {
+        backgroundColor: '#111111',
+        borderBottomWidth: 0
+      },
+      headerTintColor: '#fff',
+      headerTitle: (
+        <View style={{flex:1, flexDirection:'row', justifyContent:'center'}}>
+          <Image
+            source={require('AretsDagar/assets/logo.png')}
+            style={{ width:100, height: 35, paddingBottom: 3 }}
+          />
+        </View>
+      ),
     }),
   },
   Tradition: {
     screen: Tradition,
     navigationOptions: ({ navigation }) => ({
       title: navigation.state.params.title,
-      headerBackTitle: 'Tillbaka' // @TODO Doesn't work...
+      headerStyle: {
+        backgroundColor: '#111111',
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        fontWeight: 'bold',
+      },
     }),
   }
 }, {initialRouteName: 'Calendar'});
+
+const AlphabeticStack = createStackNavigator({
+  Alphabetic: {
+    screen: Alphabetic,
+    navigationOptions: () => ({
+      title: 'Alphabetic',
+      headerBackTitle: 'Tillbaka',
+      headerStyle: {
+        backgroundColor: '#111111',
+        borderBottomWidth: 0
+      },
+      headerTintColor: '#fff',
+      headerTitle: (
+        <View style={{flex:1, flexDirection:'row', justifyContent:'center'}}>
+          <Image
+            source={require('AretsDagar/assets/logo.png')}
+            style={{ width:100, height: 35, paddingBottom: 3 }}
+          />
+        </View>
+      ),
+    }),
+  },
+  Tradition: {
+    screen: Tradition,
+    navigationOptions: ({ navigation }) => ({
+      title: navigation.state.params.title,
+      headerStyle: {
+        backgroundColor: '#111111',
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        fontWeight: 'bold',
+      },
+    }),
+  }
+}, {initialRouteName: 'Alphabetic'});
 
 const RouteConfigs = {
   Calendar: {
     screen: CalendarStack,
     navigationOptions: () => ({
-      title: 'Dagar'
+      tabBarLabel: 'Dagar',
+      tabBarIcon: ({tintColor}) => (
+        <Icon name="date-range" color={tintColor} size={24} />
+      )
     }),
   },
   Alphabetic: {
-    screen: Alphabetic,
+    screen: AlphabeticStack,
     navigationOptions: () => ({
-      title: 'Lista A - Ö'
+      tabBarLabel: 'Lista A - Ö',
+      tabBarIcon: ({tintColor}) => (
+        <Icon name="view-list" color={tintColor} size={24} />
+      )
     }),
   },
   Reminders: {
     screen: Reminders,
     navigationOptions: () => ({
-      title: 'Påminnelser'
+      tabBarLabel: 'Påminnelser',
+      tabBarIcon: ({tintColor}) => (
+        <Icon name="alarm" color={tintColor} size={24} />
+      )
     }),
   },
   Info: {
     screen: Info,
     navigationOptions: () => ({
-      title: 'Info'
+      tabBarLabel: 'Info',
+      tabBarIcon: ({tintColor}) => (
+        <Icon name="info" color={tintColor} size={24} />
+      )
     }),
   },
 };
@@ -113,17 +178,20 @@ const TabNavigatorConfig = {
   lazy: true,
   animationEnabled: false,
   tabBarOptions: {
-    activeTintColor: '#e26901',
+    activeTintColor: '#f2d49c',
     labelStyle: {
       fontSize: 12,
     },
     allowFontScaling: false,
+    style: {
+      backgroundColor: '#111111',
+    }
   },
 };
 
 const baseStyles = {
   backgroundColor: '#1d1d1d',
-  flex: 1,
+  flex: 1
 }
 
 const TabsView = createBottomTabNavigator(RouteConfigs, TabNavigatorConfig);
