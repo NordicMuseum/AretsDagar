fb936d015e2a0f8d1330defe57c7bb2e58129fdc
Viktor Miranda <info@viktormiranda.com>
2019-03-01 00:09:34 +0100

    Added geo position and functionality to celebrations

--- android/app/src/main/java/com/aretsdagar/MainApplication.java
+++ android/app/src/main/java/com/aretsdagar/MainApplication.java
@@ -10,7 +10,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.airbnb.android.react.maps.MapsPackage;
+import com.agontuk.RNFusedLocation.RNFusedLocationPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -27,9 +27,10 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new MapsPackage(),
-            new VectorIconsPackage(),
-            new RNGestureHandlerPackage()
+          new MapsPackage(),
+          new VectorIconsPackage(),
+          new RNGestureHandlerPackage(),
+          new RNFusedLocationPackage()
       );
     }
 
